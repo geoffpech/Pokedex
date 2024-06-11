fa7eaef7aed9dc69a61cc5a2ab7b27a65972d2a8
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-06-11 16:45:23 +0200

    pokedex react 03

--- src/components/Pokemon.Card.jsx
+++ src/components/Pokemon.Card.jsx
@@ -1,7 +1,20 @@
 export default function PokemonCard () {
+    const pokemon = pokemonList[0];
+
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="photo bulbasaur"/>
-        <figcaption>Bulbasaur</figcaption>
+       {pokemon.imgSrc? <img src={pokemon.imgSrc} alt="photo bulbasaur"/> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
-}
+        }
+
+    const pokemonList = [
+        {
+          name: "bulbasaur",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+      ];
 
