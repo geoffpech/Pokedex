0bf229f31af74f660e7c81a6c6bc13d6ddf53e1e
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-06-11 15:36:13 +0200

    challenge react02

---
+++ src/components/Pokemon.Card.jsx
@@ -0,0 +1,7 @@
+export default function PokemonCard () {
+    return <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="photo bulbasaur"/>
+        <figcaption>Bulbasaur</figcaption>
+    </figure>
+}
+
